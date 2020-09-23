62825a5d9edf9478f47abb8a89db9fa288091e6c
mchandonnet <mike@chandon.net>
2020-09-23 16:24:31 -0400

    Fixed config.js removed trailing "/" from API URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://boiling-reef-95188.herokuapp.com/',
+  production: 'https://boiling-reef-95188.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
